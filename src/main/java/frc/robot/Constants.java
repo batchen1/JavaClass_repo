3cf826ecedd553b9dea588464eee3ff464246a02
CoolName <wessjackson93@gmail.com>
2024-11-25 20:04:49 -0500

    ILOVECHEESE

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -12,8 +12,10 @@ package frc.robot;
  * <p>It is advised to statically import this class (or one of its inner classes) wherever the
  * constants are needed, to reduce verbosity.
  */
+//jj
 public final class Constants {
   public static class OperatorConstants {
     public static final int kDriverControllerPort = 0;
+  public int change;
   }
 }
